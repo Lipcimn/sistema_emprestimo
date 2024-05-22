43d86f84f6c9f71557d3a4478b6ce8f0b9b96ab7
unisanta <unisanta@santacecilia.br>
2024-05-22 19:56:45 -0300

    alterações em terminal + outros arquivos js/ts deletados

--- src/terminal.ts
+++ src/terminal.ts
@@ -1,15 +1,20 @@
+import promptSync from "prompt-sync"
 import { Usuario, Emprestimo, sistema} from "./emprestimo";
 
-sistema.novoUsuario(new Usuario("pedro", "123"));
-sistema.novoUsuario(new Usuario("maria", "456"));
-sistema.novoUsuario(new Usuario("joao", "789"));
-sistema.novoEmprestimo(
-  new Emprestimo(new Date(), 100, sistema.usuarios[0], sistema.usuarios[1])
-);
-sistema.novoEmprestimo(
-  new Emprestimo(new Date(), 100, sistema.usuarios[0], sistema.usuarios[1])
-);
-sistema.novoEmprestimo(
-  new Emprestimo(new Date(), 100, sistema.usuarios[1], sistema.usuarios[2])
-);
-sistema.obterEmprestimos("pedro", "maria");
+const prompt = promptSync()
+
+console.log("Bem vindo ao Sistema de Empréstimo Gladious-Spei")
+let loop: string = "sim"
+while(loop === "sim"){
+console.log("Para realizar o pagamento informe a baixo o nome do credor e do devedor: ");
+let credor: string = prompt("Credor: ")
+let devedor: string = prompt("Devedor: ")
+sistema.obterEmprestimos(credor, devedor)
+let pagamentoIndex: string = prompt("Selecione o número do empréstimo a pagar: ")
+let senhaCredor: string = prompt("Informe a senha do credor: ");
+sistema.pagarEmprestimo(sistema.emprestimos[Number(pagamentoIndex)], senhaCredor)
+loop = prompt("Deseja realizar outro pagamento? (sim ou nao): ")
+if(loop === "nao"){
+  break;
+  } 
+}
